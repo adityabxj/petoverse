27794539772ff00b8b113995b26b315c146dc038
adityabxj <adityabxj@gmail.com>
2025-11-14 16:40:38 +0530

    File Structuring: Backend Added

---
+++ recordservice/src/main/java/com/petoverse/recordservice/RecordserviceApplication.java
@@ -0,0 +1,13 @@
+package com.petoverse.recordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordserviceApplication.class, args);
+	}
+
+}
